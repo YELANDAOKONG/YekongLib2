e71343194e550b4b611cca553cdc1156a9e77662
YELANDAOKONG <yelandaokong@yldk.xyz>
2023-10-24 18:21:55 +0800

    Add HMAC SHA1/256/384/512/MD5

---
+++ YekongLib2/Hash/HMACSHA256.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace YekongLib2.Hash;
+public class HMACSHA256 : IHashAlgorithm
+{
+    private System.Security.Cryptography.HMACSHA256 algorithm;
+
+    public HMACSHA256()
+    {
+        algorithm = new System.Security.Cryptography.HMACSHA256();
+    }
+
+    public static string GetString(byte[] data)
+    {
+        var tmp = new HMACSHA256();
+        tmp.Update(data);
+        return tmp.GetHash();
+    }
+    
+    public static string GetString(string data, Encoding encoding = null)
+    {
+        if (encoding == null)
+        {
+            encoding = Encoding.UTF8;
+        }
+        return GetString(encoding.GetBytes(data));
+    }
+
+    public void Update(byte[] data)
+    {
+        algorithm.TransformBlock(data, 0, data.Length, null, 0);
+    }
+
+    public void Update(byte[] data, int offset, int length)
+    {
+        algorithm.TransformBlock(data, offset, length, null, 0);
+    }
+
+    public string GetHash()
+    {
+        algorithm.TransformFinalBlock(new byte[0], 0, 0);
+        byte[] hash = algorithm.Hash;
+        algorithm.Clear();
+
+        StringBuilder sb = new StringBuilder();
+        foreach (byte b in hash)
+        {
+            sb.Append(b.ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+
+}
---
+++ YekongLib2/Hash/HMACSHA384.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace YekongLib2.Hash;
+public class HMACSHA384 : IHashAlgorithm
+{
+    private System.Security.Cryptography.HMACSHA384 algorithm;
+
+    public HMACSHA384()
+    {
+        algorithm = new System.Security.Cryptography.HMACSHA384();
+    }
+
+    public static string GetString(byte[] data)
+    {
+        var tmp = new HMACSHA384();
+        tmp.Update(data);
+        return tmp.GetHash();
+    }
+    
+    public static string GetString(string data, Encoding encoding = null)
+    {
+        if (encoding == null)
+        {
+            encoding = Encoding.UTF8;
+        }
+        return GetString(encoding.GetBytes(data));
+    }
+
+    public void Update(byte[] data)
+    {
+        algorithm.TransformBlock(data, 0, data.Length, null, 0);
+    }
+
+    public void Update(byte[] data, int offset, int length)
+    {
+        algorithm.TransformBlock(data, offset, length, null, 0);
+    }
+
+    public string GetHash()
+    {
+        algorithm.TransformFinalBlock(new byte[0], 0, 0);
+        byte[] hash = algorithm.Hash;
+        algorithm.Clear();
+
+        StringBuilder sb = new StringBuilder();
+        foreach (byte b in hash)
+        {
+            sb.Append(b.ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+
+}
